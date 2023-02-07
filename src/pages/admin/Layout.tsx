44872c925e58dcdaac83036abc07d1841840b678
anig1scur <anigiscur@gmail.com>
2023-02-08 02:19:24 +0800

    feat: admin panel

---
+++ src/pages/admin/Layout.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { memo } from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { AppBar, Layout, InspectorButton } from 'react-admin';
+import { Typography } from '@mui/material';
+
+const MyAppBar = memo(props => (
+    <AppBar {...props}>
+        <Typography flex="1" variant="h6" id="react-admin-title" />
+        <InspectorButton />
+    </AppBar>
+));
+
+export default props => (
+    <>
+        <Layout {...props} appBar={MyAppBar} />
+        <ReactQueryDevtools
+            initialIsOpen={false}
+            toggleButtonProps={{ style: { width: 20, height: 30 } }}
+        />
+    </>
+);
